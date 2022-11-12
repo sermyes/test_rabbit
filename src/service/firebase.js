d1f27fdbbcc5eacf5ca7bb301765a153eaea71eb
lsh <sermyes@gmail.com>
2022-11-12 21:42:24 +0900

    test

--- src/service/firebase.js
+++ src/service/firebase.js
@@ -1,10 +1,12 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js';
 import * as FIREBASE from '../../env.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const firebaseConfig = {
-  apiKey: FIREBASE.API_KEY,
-  databaseURL: FIREBASE.DB_URL,
-  projectId: FIREBASE.PROJECT_ID
+  apiKey: process.env.API_KEY,
+  databaseURL: process.env.DB_URL,
+  projectId: process.env.PROJECT_ID
 };
 
 // Initialize Firebase
