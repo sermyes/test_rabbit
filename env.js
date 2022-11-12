184517a0c3e281d90a1435a59e86faf3ed3d6f05
lsh <sermyes@gmail.com>
2022-11-12 21:22:41 +0900

    test

---
+++ env.js
@@ -0,0 +1,3 @@
+export const API_KEY = 'AIzaSyD9FrJ-bsNIIkl6ld52vucKIEoe2QzLJvo';
+export const DB_URL = 'https://vegetable-game-default-rtdb.firebaseio.com';
+export const PROJECT_ID = 'vegetable-game';
